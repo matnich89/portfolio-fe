6d6a4da2e9c2a44aa676c56eb24cc351af20afe3
mathewnicholls <mathewnicholls@yandex.com>
2024-06-28 21:21:54 +0100

    inital static  implementation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import "../app.css";
+	import { page } from '$app/stores';
+</script>
+
+<nav class="flex justify-center my-6">
+	<ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg::mt-0 text-lg">
+		<a href="#" class="step step-primary">
+			Who am I?
+		</a>
+		<a href="#">
+			My Tech Stack
+		</a>
+		<a href="#">
+      My Projects
+		</a>
+	</ul>
+</nav>
+
+<div class="min-h-screen flex flex-col">
+	<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<div class="container mx-auto px-4 py-8">
+	<h1 class="text-center text-4xl text-white-800 mb-6">
+		<img src="/rust.svg" alt="rust" class="inline-block h-8 w-8 align-middle ml-2" />
+		Mat Nicholls Software Engineer Who Explores
+		<img src="/go.svg" alt="go gopher" class="inline-block h-8 w-8 align-middle ml-2" />
+	</h1>
+
+	<section class="my-8">
+		<div class="bg-base-200 p-6 rounded-lg shadow-lg">
+			<h2 class="text-3xl font-bold text-white mb-4">About Me</h2>
+			<p class="text-white-700 text-lg leading-relaxed">
+				Hello! I'm Mat Nicholls, a polyglot software engineer experienced in microservices, DevOps, Kubernetes, and associated technologies. I specialize in Kotlin, Go, and Rust development, with exposure to JavaScript (mostly via React) and Python. With a solid foundation in full-stack development and operations, I can effectively manage and deploy scalable applications.
+			</p>
+			<p class="text-white-700 text-lg leading-relaxed mt-4">
+				I have a strong background in designing and implementing microservices architectures, leveraging the power of Kubernetes for orchestration and deployment. My expertise in DevOps practices ensures seamless integration and continuous delivery, enabling efficient and reliable software delivery pipelines.
+			</p>
+			<p class="text-white-700 text-lg leading-relaxed mt-4">
+				Whether it's developing robust backend services, creating dynamic frontend experiences, or managing cloud infrastructure, I bring a comprehensive skill set to the table. Let's connect and discuss how I can contribute to your next project!
+			</p>
+		</div>
+	</section>
+
+</div>
